1cb0653c299a139398dd9ecbc44dbfe9a43353a1
unknown <you@example.com>
2024-01-19 06:48:51 +0600

    first commit

--- FashionPark/settings.py
+++ FashionPark/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'rest_framework',
-    'rest_framework.authtoken',
     'django_filters',
     'user',
     'core',
